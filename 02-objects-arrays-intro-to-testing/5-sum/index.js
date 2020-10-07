8289a14a0e78ff23990a1846214042d198767d06
Alex <ilovecoding.start@gmail.com>
2020-10-08 00:52:12 +0300

    sum func implementation

--- 02-objects-arrays-intro-to-testing/5-sum/index.js
+++ 02-objects-arrays-intro-to-testing/5-sum/index.js
@@ -3,6 +3,10 @@
  * @param {number} n value
  * @returns {number | function}
  */
-export function sum (n) {
-
+export function sum(n = 0) {
+  const add = m => m === undefined ? sum(n) : sum(n + m);
+  add.valueOf = add.toString = () => {
+    return n;
+  };
+  return add;
 }
